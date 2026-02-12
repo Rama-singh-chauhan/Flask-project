8dd96dbbfaf535f50b131b44924c08ffd4c26a94
Rama Chauhan <ramachauhan@Ramas-MacBook-Air.local>
2026-02-12 09:05:02 -0800

    Initial commit

---
+++ file.py
@@ -0,0 +1,6 @@
+file = open("sample.txt", "w")
+file.write("Hello, this is a sample file.\n")
+file.write("We are learning file handling in Python.")
+file.close()
+
+print("Content written to file successfully.")
